352b28805c09c28a9cfa3573214612974f3ddfca
melihyelman <myelman17@gmail.com>
2021-08-02 23:09:44 +0300

    uploade image module add

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const fs = require('fs');
 const ejs = require('ejs');
+const fileUpload = require('express-fileupload');
 const path = require('path');
 
 const Photo = require('./models/Photo');
@@ -10,10 +12,12 @@ const app = express();
 
 //connect db
 
-mongoose.connect('mongodb://localhost/pcat-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect('mongodb://localhost/pcat-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => console.log(err));
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -22,6 +26,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 //ROUTES
 app.get('/', async (req, res) => {
@@ -49,8 +54,22 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
